b8d3850acac070116d4b24c8049d66bdf4ebcb0a
Joe Valookaran <joevalookaran@hotmail.com>
2022-02-17 06:53:04 +0000

    created a cart in the database and an ajax post request

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,17 +1,81 @@
 $(document).ready(function() {
-$("#cart_display").click(function(){
-$("#cart").toggle();
-$(".menu").click(function(event){
- $(".toppings").show();
- $("input#item_id").val(this.id);
-  alert(this.id);
-})
-});
+  $("#cart_display").click(function(){
+    $("#cart").toggle();
+
+  $(".menu").click(function(event){
+    $(".toppings").show();
+    $("input#item_id").val(this.id);
+    alert(this.id);
+  })
+  });
 
 
-console.log("loading app.js");
+  console.log("loading app.js");
+
+  const createCartElement = function(item) {
+
+    const content = `<img src=${item.photo_url} width="200"
+    height="200"><h3>${item.name} </h3><p>${item.description}</p><p>$ ${item.price_cents/Math.pow(10, 2)}</p>`;
+    const output = `<article class='menu' id='${item.id}'>${content}</article>`;
+    return output;
+  };
+
+  const appendTweetElement = (tweetData) => {
+    $('#tweetBox').append(createTweetElement(tweetData));
+  }
+
+  const loadTweets = function(url) {
+    $.get(url).then((data) => {
+      renderTweets(data);
+  })
+  }
 
 
+  const createTweetElement = (tweetData) => {
+    const layout = `
+      <article class="tweets" id="boxShadow">
+        <header class="oppositeSide ">
+          <div class="picName">
+            <div class="imageBox" >
+              <img src="${tweetData.user.avatars}" alt="Random Man Face">
+            </div>
+            <div >
+              <p style = "margin-left: 10px;">${tweetData.user.name}</p>
+            </div>
+          </div>
+          <div class="handle">
+            <p>${tweetData.user.handle}</p>
+          </div>
+
+        </header>
+        <p class="tweetContainer">
+        ${(htmlEncode(tweetData.content.text))}
+        </p>
+        <footer class = "oppositeSide small">
+          <span> ${timeago.format(Number(tweetData.created_at))}</span>
+          <div class = "horizontal">
+            <i class="fa-solid fa-flag icon1"></i>
+            <i class="fa-solid fa-retweet icon2"></i>
+            <i class="fa-solid fa-heart icon3"></i>
+          </div>
+        </footer>
+      </article>
+    `
+    return layout
+  }
+  const $form = $('#menuForm');
+  $form.on('submit', function (e) {
+    console.log('form submitted!');
+    let formValues = $(this).serialize();
+    $.post("/carts2", formValues,() => {
+      console.log('ajax post request went through');
+      // loadRecentTweet("/tweets/")
+      // document.getElementById('counter').innerHTML = '140';
+    });
+    // make textbox empty again
+    $form[0].reset();
+
+  })
   //generate each item in the array
   const renderItems = function(items) {
 
--- public/scripts/database.js
+++ public/scripts/database.js
@@ -32,10 +32,59 @@ exports.getPhoneNumber = getPhoneNumber;
 const createCartid = () => {
   return pool
     .query(`SELECT COUNT(*) FROM carts `)
-    .then((result) => result.rows[0].count)
+    .then((result) => Number(result.rows[0].count) + 1)
     .catch((err) => {
       return (err.message);
     });
 };
 exports.createCartid = createCartid;
-createCartid();
+
+// const createCartsql = (user_id, cart_id) => {
+//   return pool
+//     .query(`INSERT INTO carts(cart_id, user_id, time_started) VALUES($2, $1, CURRENT_TIMESTAMP) RETURNING *;`, [user_id, cart_id])
+//     .then((result) => console.log(result.rows))
+//     .catch((err) => {
+//       return (err.message);
+//     });
+// };
+// exports.createCartsql = createCartsql;
+
+
+const createCartsql = (user_id, cart_id) => {
+  pool.query(
+    `INSERT INTO carts(id, user_id, time_started) VALUES($2, $1, CURRENT_TIMESTAMP) RETURNING *;`, [user_id, cart_id],
+    (err, res) => {
+      console.log(err, res.rows);
+      pool.end();
+    }
+  );
+}
+exports.createCartsql = createCartsql;
+
+// createCartid()
+//   .then(res => createCartsql(10, res));
+// const addProperty = (propertyObj) => {
+//   return pool
+//     .query(`
+//     INSERT INTO properties(owner_id,
+//       title,
+//       description,
+//       thumbnail_photo_url,
+//       cover_photo_url,
+//       cost_per_night,
+//       street,
+//       city,
+//       province,
+//       post_code,
+//       country,
+//       parking_spaces,
+//       number_of_bathrooms,
+//       number_of_bedrooms)
+//     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+//     RETURNING *;
+//     `, [propertyObj.owner_id, propertyObj.title, propertyObj.description, propertyObj.thumbnail_photo_url, propertyObj.cover_photo_url, propertyObj.cost_per_night, propertyObj.street, propertyObj.city, propertyObj.province, propertyObj.post_code, propertyObj.country, propertyObj.parking_spaces, propertyObj.number_of_bathrooms, propertyObj.number_of_bedrooms])
+//     .then((result) => result.rows[0])
+//     .catch((err) => {
+//       return (err.message);
+//     });
+// };
